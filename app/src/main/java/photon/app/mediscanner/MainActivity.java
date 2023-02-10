b55314df831ba37627ae9b0fe4c773827f901e7a
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2023-02-10 12:19:50 +0530

    by rakesh

--- app/src/main/java/photon/app/mediscanner/MainActivity.java
+++ app/src/main/java/photon/app/mediscanner/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // now checking by raghu
+        // now checking by rakesh
     }
 }
